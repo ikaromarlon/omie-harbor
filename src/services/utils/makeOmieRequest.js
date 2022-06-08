dbc27a4cae196ca7bafdc4e69bb67b2e414afe17
Icaro Silva <ikaromarlon@gmail.com>
2022-06-08 10:13:20 -0300

    feat: remove promise.all from makeOmieRequest

--- src/services/utils/makeOmieRequest.js
+++ src/services/utils/makeOmieRequest.js
@@ -6,17 +6,9 @@ module.exports = async ({ requester, method, url, body, propertiesMapping }) => 
 
   responses.push(await requester[method](url, body))
 
-  if (config.services.omie.waitSecondsBeforeNextPageRequest) {
-    for (let i = responses[0].data[propertiesMapping.pagination.currentPage] + 1; i <= responses[0].data[propertiesMapping.pagination.totalPages]; i++) {
-      await sleep(config.services.omie.waitSecondsBeforeNextPageRequest)
-      responses.push(await requester[method](url, { ...body, param: [{ ...body.param[0], [propertiesMapping.pagination.currentPage]: i }] }))
-    }
-  } else {
-    const requests = []
-    for (let i = responses[0].data[propertiesMapping.pagination.currentPage] + 1; i <= responses[0].data[propertiesMapping.pagination.totalPages]; i++) {
-      requests.push(requester[method](url, { ...body, param: [{ ...body.param[0], [propertiesMapping.pagination.currentPage]: i }] }))
-    }
-    responses.push(...(await Promise.all(requests)))
+  for (let i = responses[0].data[propertiesMapping.pagination.currentPage] + 1; i <= responses[0].data[propertiesMapping.pagination.totalPages]; i++) {
+    await sleep(config.services.omie.waitSecondsBeforeNextPageRequest)
+    responses.push(await requester[method](url, { ...body, param: [{ ...body.param[0], [propertiesMapping.pagination.currentPage]: i }] }))
   }
 
   return responses.flatMap(e => e.data[propertiesMapping.data])
