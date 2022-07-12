7da0b89730043c598c5717b184f27b7ec7b879cb
Icaro Silva <ikaromarlon@gmail.com>
2022-07-12 20:36:49 -0300

    refactor: change some logics in responseHandler util

--- src/utils/responseHandler.js
+++ src/utils/responseHandler.js
@@ -3,19 +3,17 @@ const mailer = require('./mailer')
 const logger = require('./logger')
 
 module.exports = (data, headers) => {
-  const response = (statusCode, responseData) => {
-    return {
-      statusCode,
-      body: JSON.stringify(responseData),
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Credentials': true,
-        ...(headers || {})
-      }
+  const response = (statusCode, responseData) => ({
+    statusCode,
+    body: JSON.stringify(responseData),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Credentials': true,
+      ...(headers || {})
     }
-  }
+  })
 
   return {
     success: (statusCode = 200) => {
@@ -23,14 +21,15 @@ module.exports = (data, headers) => {
       return response(statusCode, data)
     },
     error: async (statusCode = 500) => {
-      const { message, stack } = data
-      const error = { message, ...data, stack }
+      const { message, stack, ...otherData } = data
+      /** Error classes (data object) does not keep message and stack properties in JSON.stringify */
+      const error = { message, stack, ...otherData }
       logger.error({ title: 'PROCESS ENDED WITH ERROR', message, data: error })
-      if (config.services.mailer.errorNotificationRecipientAddres) {
+      if (config.services.mailer.errorNotificationRecipientAddress) {
         await mailer().sendErrorNotification(error)
       }
       if (config.app.stage === 'prd') {
-        return response(statusCode, { message })
+        return response(statusCode, { message, ...otherData })
       }
       return response(statusCode, error)
     }
