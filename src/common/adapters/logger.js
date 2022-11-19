1a92e01f6225eb5857b9c5acbec5781cec382fba
Icaro Silva <ikaromarlon@gmail.com>
2022-11-19 11:21:11 -0300

    refactor: logger adapter

--- src/common/adapters/logger.js
+++ src/common/adapters/logger.js
@@ -1,4 +1,6 @@
+const log = ({ title, message, data }) => `[${process.env.UUID}]${title ? ` ${title}:` : ''}${message ? ` ${message}` : ''}\n${JSON.stringify({ processId: process.env.UUID, timestamp: new Date(), message, data }, null, 2)}`
+
 module.exports = {
-  info: ({ title, message, data }) => console.info(`[${process.env.UUID}]${title ? ` ${title}:` : ''}${message ? ` ${message}` : ''}\n${JSON.stringify({ processId: process.env.UUID, timestamp: new Date(), message, data }, null, 2)}`.trim()),
-  error: ({ title, message, data }) => console.error(`[${process.env.UUID}]${title ? ` ${title}:` : ''}${message ? ` ${message}` : ''}\n${JSON.stringify({ processId: process.env.UUID, timestamp: new Date(), message, data }, null, 2)}`.trim())
+  info: ({ title, message, data }) => console.info(log({ title, message, data })),
+  error: ({ title, message, data }) => console.error(log({ title, message, data }))
 }
