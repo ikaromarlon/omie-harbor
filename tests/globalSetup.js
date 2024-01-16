4c3bc93feb2d1e24a6cea33d9df0d7f477b75471
Icaro Silva <ikaromarlon@gmail.com>
2024-01-16 19:15:16 +0100

    ci: setup integration tests env vars

--- tests/globalSetup.js
+++ tests/globalSetup.js
@@ -1,3 +1,11 @@
-require('dotenv').config({ path: '.env.test' })
+const dotenv = require('dotenv')
+const fs = require('node:fs')
+const { resolve } = require('node:path')
+
+const envFile = resolve(__dirname, '..', '.env.test')
+
+if (fs.existsSync(envFile)) {
+  dotenv.config({ path: envFile })
+}
 
 module.exports = async () => {}
