118e92af114d3ab17ee65b27b0a59a913d366b2c
Icaro Silva <ikaromarlon@gmail.com>
2024-01-16 18:47:58 +0100

    chore: increase integration tests timeouts

--- tests/config.integration.js
+++ tests/config.integration.js
@@ -2,7 +2,7 @@ const globalConfig = require('../jest.config')
 
 module.exports = {
   ...globalConfig,
-  testTimeout: 10000,
+  testTimeout: 30000,
   globalSetup: '<rootDir>/tests/globalSetup.integration.js',
   roots: [
     'src',
